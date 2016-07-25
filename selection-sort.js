182eec532d52769658be9e29d608504073e19c0f
Amir Ghafouri <aghafour@uwo.ca>
2016-07-25 14:16:38 -0400

    Create selection-sort.js

---
+++ selection-sort.js
@@ -0,0 +1,39 @@
+var swap = function(array, firstIndex, secondIndex) {
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+};
+
+var indexOfMinimum = function(array, startIndex) {
+
+    var minValue = array[startIndex];
+    var minIndex = startIndex;
+
+    for(var i = minIndex + 1; i < array.length; i++) {
+        if(array[i] < minValue) {
+            minIndex = i;
+            minValue = array[i];
+        }
+    } 
+    return minIndex;
+}; 
+
+var selectionSort = function(array) {
+    var minIndex;
+    for (var j=0; j<array.length; j++) {
+        minIndex = indexOfMinimum(array, j);
+        swap(array, j, minIndex);
+    }
+};
+
+var array = [22, 11, 99, 88, 9, 7, 42];
+selectionSort(array);
+println("Array after sorting:  " + array);
+
+Program.assertEqual(array, [7, 9, 11, 22, 42, 88, 99]);
+
+var array = [90, 38,-24, 0, 101];
+selectionSort(array);
+println("Array after sorting:  " + array);
+
+Program.assertEqual(array, [-24, 0, 38, 90, 101]);
